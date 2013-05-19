4932020ea21879ba7f48af31dcf90c8c602680b0
shantesh <shantesh@shubhalabha.in>
2013-05-19 09:51:48 +0530

    Today First

--- Shubha RT/MainWindow.xaml.cs
+++ Shubha RT/MainWindow.xaml.cs
@@ -82,6 +82,56 @@ namespace StockD
                 txtTargetFolder.Text = t1.target;
                 dtStartDate.Text = t1.fromdate.ToShortDateString();
                 dtEndDate.Text = t1.todate.ToShortDateString();
+                Cb_BSE_CASH_MARKET.IsChecked =  t1.Cb_BSE_CASH_MARKET;
+
+
+             
+Cb_BSE_Equity_Futures.IsChecked=t1.Cb_BSE_Equity_Futures;
+ChkBSEEquity.IsChecked=t1.ChkBSEEquity;
+ChkBseFo.IsChecked=t1.ChkBseFo;
+
+
+Cb_NSE_CASH_MARKET.IsChecked=t1.Cb_NSE_CASH_MARKET;
+Cb_NSE_Equity_Futures.IsChecked=t1.Cb_NSE_Equity_Futures;
+chkEquity.IsChecked=t1.chkEquity;
+Cb_NSE_Forex_Options.IsChecked=t1.Cb_NSE_Forex_Options;
+Cb_NSE_SME.IsChecked=t1.Cb_NSE_SME;
+ Cb_NSE_ETF.IsChecked=t1.Cb_NSE_ETF;
+Cb_NSE_Index.IsChecked=t1.Cb_NSE_Index;
+ Cb_Reports.IsChecked=t1.Cb_Reports;
+ chkCombinedReport.IsChecked=t1.chkCombinedReport;
+ chkNseForex.IsChecked=t1.chkNseForex;
+ chkNseNcdex.IsChecked=t1.chkNseNcdex;
+     
+
+
+ MCXSX_Forex_Future.IsChecked=t1.MCXSX_Forex_Future;
+ MCXSX_Equity_Futures.IsChecked=t1.MCXSX_Equity_Futures;
+ MCXCommodity_Futures.IsChecked=t1.MCXCommodity_Futures;
+ MCXSX_Equity_Options.IsChecked=t1.MCXSX_Equity_Options;
+ MCXSXForex_Options.IsChecked=t1.MCXSXForex_Options;
+ National_Spot_Exchange.IsChecked=t1.National_Spot_Exchange;
+ MCXSX_Equity_Indices.IsChecked=t1.MCXSX_Equity_Indices;
+  MCX_Index.IsChecked=t1.MCX_Index;
+
+
+ chkYahooEOD.IsChecked=t1.chkYahooEOD;
+ ChkYahooIEOD1.IsChecked=t1.ChkYahooIEOD1;
+ chkYahooFundamental.IsChecked=t1.chkYahooFundamental;
+ ChkYahooIEOD5.IsChecked=t1.ChkYahooIEOD5;
+ Cb_Yahoo_Realtime.IsChecked=t1.Cb_Yahoo_Realtime;
+
+ ChkGoogleEOD.IsChecked=t1.ChkGoogleEOD;
+ ChkGoogleIEOD.IsChecked=t1.ChkGoogleIEOD;
+ Cb_MCX_Google_IEOD_5min.IsChecked=t1.Cb_MCX_Google_IEOD_5min;
+
+
+ Cb_Corporate_Events.IsChecked=t1.Cb_Corporate_Events;
+ Cb_Board_Message.IsChecked=t1.Cb_Board_Message;
+ Cb_Delete_all_events.IsChecked=t1.Cb_Delete_all_events;
+
+
+               
                 fs.Close();
 
 
@@ -117,7 +167,53 @@ namespace StockD
                 t.fromdate = Convert.ToDateTime(dtStartDate.Text);
                 t.todate = Convert.ToDateTime(dtEndDate.Text);
                 t.target = txtTargetFolder.Text;
+                
+                t.Cb_BSE_CASH_MARKET=Cb_BSE_CASH_MARKET.IsChecked.Value ;
+t.Cb_BSE_Equity_Futures=Cb_BSE_Equity_Futures.IsChecked.Value;
+t.ChkBSEEquity=ChkBSEEquity.IsChecked.Value;
+t.ChkBseFo=ChkBseFo.IsChecked.Value;
 
+
+t.Cb_NSE_CASH_MARKET=Cb_NSE_CASH_MARKET.IsChecked.Value;
+t.Cb_NSE_Equity_Futures=Cb_NSE_Equity_Futures.IsChecked.Value;
+t.chkEquity=chkEquity.IsChecked.Value;
+t.Cb_NSE_Forex_Options=Cb_NSE_Forex_Options.IsChecked.Value;
+t.Cb_NSE_SME=Cb_NSE_SME.IsChecked.Value;
+t.Cb_NSE_ETF = Cb_NSE_ETF.IsChecked.Value;
+t.Cb_NSE_Index= Cb_NSE_Index.IsChecked.Value;
+t.Cb_Reports= Cb_Reports.IsChecked.Value;
+t.chkCombinedReport= chkCombinedReport.IsChecked.Value;
+t.chkNseForex= chkNseForex.IsChecked.Value;
+t.chkNseNcdex= chkNseNcdex.IsChecked.Value;
+     
+
+
+t.MCXSX_Forex_Future= MCXSX_Forex_Future.IsChecked.Value;
+t.MCXSX_Equity_Futures = MCXSX_Equity_Futures.IsChecked.Value;
+t.MCXCommodity_Futures= MCXCommodity_Futures.IsChecked.Value;
+t.MCXSX_Equity_Options= MCXSX_Equity_Options.IsChecked.Value;
+t.MCXSXForex_Options= MCXSXForex_Options.IsChecked.Value;
+t.National_Spot_Exchange= National_Spot_Exchange.IsChecked.Value;
+t.MCXSX_Equity_Indices= MCXSX_Equity_Indices.IsChecked.Value;
+t.MCX_Index=  MCX_Index.IsChecked.Value;
+
+
+t.chkYahooEOD= chkYahooEOD.IsChecked.Value;
+t.ChkYahooIEOD1= ChkYahooIEOD1.IsChecked.Value;
+t.chkYahooFundamental= chkYahooFundamental.IsChecked.Value;
+t.ChkYahooIEOD5= ChkYahooIEOD5.IsChecked.Value;
+t.Cb_Yahoo_Realtime= Cb_Yahoo_Realtime.IsChecked.Value;
+
+t.ChkGoogleEOD= ChkGoogleEOD.IsChecked.Value;
+t.ChkGoogleIEOD= ChkGoogleIEOD.IsChecked.Value;
+t.Cb_MCX_Google_IEOD_5min= Cb_MCX_Google_IEOD_5min.IsChecked.Value;
+
+
+t.Cb_Corporate_Events= Cb_Corporate_Events.IsChecked.Value;
+t.Cb_Board_Message= Cb_Board_Message.IsChecked.Value;
+t.Cb_Delete_all_events= Cb_Delete_all_events.IsChecked.Value;
+                
+                
                 BinaryFormatter bf = new BinaryFormatter();
                 FileStream fs = new FileStream(@"C:\Fileio.txt", FileMode.Create, FileAccess.Write);
                 bf.Serialize(fs, t);
@@ -151,6 +247,52 @@ namespace StockD
         public string target;
         public DateTime fromdate;
         public DateTime todate;
-        public string checkboxevent;
+        public bool  checkboxevent;
+
+
+       public bool Cb_BSE_CASH_MARKET;
+public bool Cb_BSE_Equity_Futures;
+public bool ChkBSEEquity;
+public bool ChkBseFo;
+
+
+public bool Cb_NSE_CASH_MARKET;
+public bool Cb_NSE_Equity_Futures;
+public bool chkEquity;
+public bool Cb_NSE_Forex_Options;
+public bool Cb_NSE_SME;
+public bool Cb_NSE_ETF;
+public bool Cb_NSE_Index;
+public bool Cb_Reports;
+public bool chkCombinedReport;
+public bool chkNseForex;
+public bool chkNseNcdex;
+
+
+
+public bool MCXSX_Forex_Future;
+public bool MCXSX_Equity_Futures;
+public bool MCXCommodity_Futures;
+public bool MCXSX_Equity_Options;
+public bool MCXSXForex_Options;
+public bool National_Spot_Exchange;
+public bool MCXSX_Equity_Indices;
+public bool MCX_Index;
+
+
+public bool chkYahooEOD;
+public bool ChkYahooIEOD1;
+public bool chkYahooFundamental;
+public bool ChkYahooIEOD5;
+public bool Cb_Yahoo_Realtime;
+
+public bool ChkGoogleEOD;
+public bool ChkGoogleIEOD;
+public bool Cb_MCX_Google_IEOD_5min;
+
+
+public bool Cb_Corporate_Events;
+public bool Cb_Board_Message;
+public bool Cb_Delete_all_events;
     }
 }
